156caf737383284bd0e77447c9a714cf7b383b4b
TevinPulley <tevinpulley@gmail.com>
2022-02-26 23:12:14 -0700

    final changes

--- password.js
+++ password.js
@@ -12,13 +12,13 @@ const reader = readline.createInterface({
 reader.question("What is the password?", function(input){
 	let password = ('');
 
-for (i=0; i=0; i++); {
+
     if(input.length < 9) {
         console.log('This password is too short.')
     }
     if (input.length >= 9){
         console.log('This password is just right.');
     }
-}
+
 reader.close()
 })
